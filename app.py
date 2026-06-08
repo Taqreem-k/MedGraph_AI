7d1eae60d4929e36fdb57645ce3d4f1903a07f7b
Taqreem-k <md.taqreemkhan@gmail.com>
2026-06-08 23:19:27 +0530

    feat: add page config

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
 import streamlit as st
 
+st.set_page_config(
+    page_title="MedGraph AI",
+    layout = "wide",
+    
+)
+
 st.title("MedGraph AI")
 
 st.subheader("An AI EMR System for medical data in a graphical way")
