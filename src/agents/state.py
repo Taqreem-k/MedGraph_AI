9c1aed30bba6b4f34a9816f7c0f3285db9adf586
Taqreem-k <md.taqreemkhan@gmail.com>
2026-06-20 23:39:54 +0530

    feat: add YpedDict Agent State

--- src/agents/state.py
+++ src/agents/state.py
@@ -2,8 +2,17 @@
 
 from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
+from typing import TypedDict, Annotated, List
+from langgraph.graph.message import add_messages
 
 load_dotenv()
 
 llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
 reviewer_llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
+
+class AgentState(TypedDict):
+    messages: Annotated[list, add_messages]
+    structured_data: dict
+    context: str
+    clinical_brief: str
+    guardrail_passed: bool
