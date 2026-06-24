9f23b31ede43d5c6b8966d178dca0cf47a6b0678
Taqreem-k <md.taqreemkhan@gmail.com>
2026-06-24 10:06:29 +0530

    feat: add retriever tool

--- src/agents/state.py
+++ src/agents/state.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 from langchain_google_genai import ChatGoogleGenerativeAI
 from typing import TypedDict, Annotated, List
 from langgraph.graph.message import add_messages
+from langchain_core.tools.retriever import create_retriever_tool
 
 load_dotenv()
 
@@ -16,3 +17,9 @@ class AgentState(TypedDict):
     context: str
     clinical_brief: str
     guardrail_passed: bool
+
+retirever_tool = create_retriever_tool(
+    #retriever,
+    name="patient_history_search",
+    description = "Use this tool to search and retrieve the patient's medical history, past diagnoses, lab results and general health records.",
+)
